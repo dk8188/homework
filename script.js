1f2e5d623cf6f859c3ee4dc9105666d7d102a5e0
Daria <dkvochko2011@yahoo.co.jp>
2019-07-16 22:32:09 +0900

    updated

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-'use strict';
 let money = +prompt("Ваш бюждет?","");
-console.log(money);
 let time = prompt("Введите дату в формате YYYY-MM-DD","");
-console.log(time);
+
 let appData = {
     budget:money,
     expenses:{},
@@ -11,17 +9,32 @@ let appData = {
     timeData:time,
     savings:false
 };
-let a1 = prompt("Введите обязательную статью расходов в этом месяце","");
-    a2 = prompt("Во сколько обойдется жилье?","");
-    a3 = prompt("Введите обязательную статью расходов в следующем месяце","");
-    a4 = prompt("Во сколько обойдется жилье?","");
-   // a5 =prompt("Ваш доход в месяц?","");
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++) {
+   let a = prompt("Введите обязательную статью расходов в этом месяце","");
+       b = prompt("Во сколько обойдется?","");
+   if ((typeof(a)) === 'string' && (typeof(a)) != null 
+      && a != '' && b !='' && a.length <50) {
+      console.log("done");
+      appData.expenses[a] = b;
+   } else { 
+      console.log("error");
+   }
+};
+appData.moneyPerDay = appData.budget/30;
 //appData.income.a5;
-alert(appData.budget/30);
-//alert(appData.budget%appData.expenses.a1);
+alert("Dailu budget:" + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+   console.log ("Minimum wage");}
+   else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+   console.log("average wage");}
+   else if (appData.moneyPerDay > 20000) {
+      console.log("high wage");}
+   else {
+      console.log("Mistake!");
+   }
+   
 
 
 
